1556ad54d5d6b3aae5d0cc0f34feec9050e1af78
shadowk1m <shadow.k1m@proton.me>
2025-07-08 20:30:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def get_config():
 
 @app.route('/health', methods=['GET'])
 def health():
-    return Response('ok', status=200)
+    return Response('OK', status=200)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port)
